cd08e6150cceb3bb87b804f99488db73bbba217b
mrsung82 <mrsung82@gmail.com>
2023-10-05 20:39:34 +0300

    Platform independent integer types h.b. added

---
+++ code/platform/DmPlatform.h
@@ -0,0 +1,36 @@
+/*
+MIT License
+Death Machine Game Engine
+Copyright (c) 2023 graveman82
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#ifndef DM_PLATFORM_H_INCL
+#define DM_PLATFORM_H_INCL
+
+// Fixed size platform independent types to store integers.
+typedef signed char DmInt8;
+typedef short DmInt16;
+typedef int DmInt32;
+
+typedef unsigned char DmUInt8;
+typedef unsigned short DmUInt16;
+typedef unsigned int DmUInt32;
+
+#endif // DM_PLATFORM_H_INCL
